88a9cdbaeca68d64df7e501b1f1c03059aaf6c28
Nazar Sergeev <privat.nazar@gmail.com>
2022-11-10 11:17:25 +0200

    lesson 1

---
+++ app.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+fs.readdir('./boys', (err, files) => {
+    for (const file of files) {
+        fs.stat(`./boys/${file}`, (err, stats) => {
+            if (err) {
+                console.log(err);
+            } else if (stats.isFile()) {
+                fs.readFile(`./boys/${file}`, (err, data) => {
+                    const peoples = JSON.parse(data);
+                    if (peoples.gender === 'female') {
+                        fs.rename(`./boys/${file}`, `./girls/${file}`, (err) => {
+                            console.log(err);
+                        });
+                    }
+                });
+            }
+
+        });
+    }
+});
+
+fs.readdir('./girls', (err, files) => {
+    for (const file of files) {
+        fs.stat(`./girls/${file}`, (err, stats) => {
+            if (err) {
+                console.log(err);
+            } else if (stats.isFile()) {
+                fs.readFile(`./girls/${file}`, (err, data) => {
+                    const users = JSON.parse(data);
+                    console.log(users);
+                    if (users.gender === 'male') {
+                        fs.rename(`./girls/${file}`, `./boys/${file}`, (err) => {
+                            console.log(err);
+                        });
+                    }
+                });
+            }
+        });
+    }
+});
+
