3e27e17a9d9ddcbfc2abe0fddd6923cc4e215806
Nazar Sergeev <privat.nazar@gmail.com>
2023-04-03 14:12:44 +0300

    lesson 2 hw

--- app.js
+++ app.js
@@ -93,94 +93,172 @@
 //     console.log('Server listen 5001');
 // });
 
-const fs = require('fs/promises');
-const path = require('path');
-const express = require('express');
-const app = express();
+// const fs = require('fs/promises');
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+//
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}))
+//
+//
+// app.get('/users', async (req, res) => {
+//     console.log('Users Endpoint!');
+//
+//     const buffer = await fs.readFile(path.join(__dirname, 'db', 'usersDb.json'));
+//     const users = JSON.parse(buffer.toString());
+//
+//     res.json(users);
+//
+// });
+//
+// app.get('/users/:userId', async (req, res) => {
+//     console.log('User by ID!');
+//     const {userId} = req.params;
+//
+//     const buffer = await fs.readFile(path.join(__dirname, 'db', 'usersDb.json'));
+//     const users = JSON.parse(buffer.toString());
+//
+//     const user = users.find((user) => user.id === +userId);
+//
+//     if (!user) {
+//         res.status(404).json(`User with id ${userId} not found! Please tru again`);
+//     } else {
+//         res.status(200).json(user);
+//     }
+//
+// });
+//
+// app.post('/users', async (req, res) => {
+//     const userInfo = req.body;
+//
+//     const buffer = await fs.readFile(path.join(__dirname, 'db', 'usersDb.json'));
+//     const users = JSON.parse(buffer.toString());
+//
+//     const newUser = {...userInfo, id: users[users.length - 1].id + 1};
+//     users.push(newUser);
+//
+//     await fs.writeFile(path.join(__dirname, 'db', 'usersDb.json'), JSON.stringify(users))
+//
+//     res.status(201).json(users);
+// });
+//
+// app.put('/users/:userId', async (req, res) => {
+//     const newUserInfo = req.body;
+//     const {userId} = req.params;
+//
+//     const buffer = await fs.readFile(path.join(__dirname, 'db', 'usersDb.json'));
+//     const users = JSON.parse(buffer.toString());
+//
+//     const index = users.findIndex((u) => u.id === +userId);
+//     if (index === -1) {
+//         return res.status(401).json(`User with id ${userId} not found! Please tru again`)
+//     }
+//
+//     users[index] = {...users[index], ...newUserInfo};
+//
+//     await fs.writeFile(path.join(__dirname, 'db', 'usersDb.json'), JSON.stringify(users))
+//
+//     res.status(200).json(users[index])
+//
+// });
+//
+// app.delete('/users/:userId', async (req, res) => {
+//     const {userId} = req.params;
+//
+//     const buffer = await fs.readFile(path.join(__dirname, 'db', 'usersDb.json'));
+//     const users = JSON.parse(buffer.toString());
+//     const index = users.findIndex((u) => u.id === +userId);
+//
+//     users.splice(index, 1);
+//     await fs.writeFile(path.join(__dirname, 'db', 'usersDb.json'), JSON.stringify(users));
+//
+//     res.sendStatus(204);
+//
+// });
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+// const fs = require('fs/promises');
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+//
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+//
+// app.get('/users', async (req, res) => {
+//     console.log('Users Endpoint!!!');
+//     const pathFile = path.join(__dirname, 'db', 'usersDb.json');
+//
+//     const buffer = await fs.readFile(pathFile);
+//     const data = JSON.parse(buffer.toString());
+//     res.json(data);
+// })
+//
+// app.get('/users/:userId', async (req, res) => {
+//     const {userId} = req.params;
+//     const pathFile = path.join(__dirname, 'db', 'usersDb.json');
+//
+//     const buffer = await fs.readFile(pathFile);
+//     const data = JSON.parse(buffer.toString());
+//     const user = data.find((u) => u.id === +userId);
+//
+//
+//     if (!user) {
+//         res.status(404).json(`User with ${userId} not found, please try again !`)
+//     }
+//     res.status(200).json(user);
+// })
+//
+// app.post('/users', async (req, res) => {
+//     const userInfo = req.body;
+//     const pathFile = path.join(__dirname, 'db', 'usersDb.json');
+//
+//     const buffer = await fs.readFile(pathFile);
+//     const data = JSON.parse(buffer.toString());
+//
+//     const newUser = {...userInfo, id: data[data.length - 1].id + 1};
+//     data.push(newUser);
+//
+//     await fs.writeFile(pathFile, JSON.stringify(data));
+//     res.json(data)
+//
+// });
+//
+// app.put('/users/:userId', async (req, res) => {
+//     const {userId} = req.params;
+//     const userInfo = req.body;
+//     const pathFile = path.join(__dirname, 'db', 'usersDb.json');
+//
+//     const buffer = await fs.readFile(pathFile);
+//     const data = JSON.parse(buffer.toString());
+//     const index = data.findIndex((u) => u.id === +userId);
+//
+//     data[index] = {...data[index], ...userInfo}
+//     await fs.writeFile(pathFile, JSON.stringify(data));
+//
+//     res.json(data[index]);
+//
+// });
+//
+// app.delete('/users/:userId', async (req, res) => {
+//     const {userId} = req.params;
+//     const pathFile = path.join(__dirname, 'db', 'usersDb.json');
+//
+//     const buffer = await fs.readFile(pathFile);
+//     const data = JSON.parse(buffer.toString());
+//
+//     const index = data.findIndex((u) => u.id === +userId);
+//     data.splice(index, 1);
+//
+//     await fs.writeFile(pathFile, JSON.stringify(data));
+//
+//     res.json(data);
+// });
 
-
-app.get('/users', async (req, res) => {
-    console.log('Users Endpoint!');
-
-    const buffer = await fs.readFile(path.join(__dirname, 'db', 'usersDb.json'));
-    const users = JSON.parse(buffer.toString());
-
-    res.json(users);
-
-});
-
-app.get('/users/:userId', async (req, res) => {
-    console.log('User by ID!');
-    const {userId} = req.params;
-
-    const buffer = await fs.readFile(path.join(__dirname, 'db', 'usersDb.json'));
-    const users = JSON.parse(buffer.toString());
-
-    const user = users.find((user) => user.id === +userId);
-
-    if (!user) {
-        res.status(404).json(`User with id ${userId} not found! Please tru again`);
-    } else {
-        res.status(200).json(user);
-    }
-
-});
-
-app.post('/users', async (req, res) => {
-    const userInfo = req.body;
-
-    const buffer = await fs.readFile(path.join(__dirname, 'db', 'usersDb.json'));
-    const users = JSON.parse(buffer.toString());
-
-    const newUser = {...userInfo, id: users[users.length - 1].id + 1};
-    users.push(newUser);
-
-    await fs.writeFile(path.join(__dirname, 'db', 'usersDb.json'), JSON.stringify(users))
-
-    res.status(201).json(users);
-});
-
-app.put('/users/:userId', async (req, res) => {
-    const newUserInfo = req.body;
-    const {userId} = req.params;
-
-    const buffer = await fs.readFile(path.join(__dirname, 'db', 'usersDb.json'));
-    const users = JSON.parse(buffer.toString());
-
-    const index = users.findIndex((u) => u.id === +userId);
-    if (index === -1) {
-        return res.status(401).json(`User with id ${userId} not found! Please tru again`)
-    }
-
-    users[index] = {...users[index], ...newUserInfo};
-
-    await fs.writeFile(path.join(__dirname, 'db', 'usersDb.json'), JSON.stringify(users))
-
-    res.status(200).json(users[index])
-
-});
-
-app.delete('/users/:userId', async (req, res) => {
-    const {userId} = req.params;
-
-    const buffer = await fs.readFile(path.join(__dirname, 'db', 'usersDb.json'));
-    const users = JSON.parse(buffer.toString());
-    const index = users.findIndex((u) => u.id === +userId);
-
-    users.splice(index, 1);
-    await fs.writeFile(path.join(__dirname, 'db', 'usersDb.json'), JSON.stringify(users));
-
-    res.sendStatus(204);
-
-});
-
-
-const port = 5000;
-app.listen(port, () => {
-    console.log(`Server listen ${port}`);
-});
+//
+// const port = 5000;
+// app.listen(port, () => {
+//     console.log(`Server listen ${port}`);
+// });
 
 
